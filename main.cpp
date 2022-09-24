01a741050e5162a9f3d6a26bf0d2d568a6c1cd4a
sficara <sficara@edmundsgovtech.com>
2022-09-24 15:01:20 -0400

    repo creation init commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+using namespace winrt;
+using namespace Windows::Foundation;
+
+int main()
+{
+    init_apartment();
+    //printf("Hello, %ls!\n", uri.AbsoluteUri().c_str());
+    
+    string filename = "";
+    while (filename != "q") {
+        cout << "Enter a path to the poem text file you would like to read or q to quit";
+
+    }
+}
